25e4fb14aa107ea02f16d8e67009fcaeac3cd4bf
Jason Deng <jdengg@umich.edu>
2021-01-10 14:59:19 -0500

    edits

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const theme = document.querySelector("#theme-link");
 // Listen for a click on the button
 btn.addEventListener("click", function() {
   // If the current URL contains "ligh-theme.css"
-  if (theme.getAttribute("href") == "style.css") {
+  if (theme.getAttribute("href") == "css/style.css") {
     // ... then switch it to "dark-theme.css"
-    theme.href = "dark.css";
+    theme.href = "css/dark.css";
   // Otherwise...
   } else {
     // ... switch it to "light-theme.css"
-    theme.href = "style.css";
+    theme.href = "css/style.css";
   }
 });
